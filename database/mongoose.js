7e52e47a4773d1303fbfdf0b77b9e9112d0d6f63
danzycool <bizwithdan@gmail.com>
2022-04-19 09:21:35 +0100

    Installed mongoose and connected to mongoDb

---
+++ database/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://127.0.0.1:27017/taskManagerDb')
+.then(()=>{
+    console.log('DB Connected Sucessfully');
+})
+.catch((error)=>{
+        console.log(error);
+    }
+);
+
+module.exports = mongoose;
